f5093bfffcb236c899dde8213a2d38223605e115
serg <serj.teplov@gmail.com>
2020-01-28 08:27:06 +0300

    zuul

--- src/main/java/ru/serj/currencyconversionservice/gateway/CurrencyExchangeServiceProxy.java
+++ src/main/java/ru/serj/currencyconversionservice/gateway/CurrencyExchangeServiceProxy.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import ru.serj.currencyconversionservice.model.CurrencyConversionBean;
 
 //@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
-@FeignClient(name = "currency-exchange-service")
-@RibbonClient(name = "currency-exchange-service")
+//@FeignClient(name = "currency-exchange-service")
+@FeignClient(name = "zuul-gateway-server")
+@RibbonClient(name = "zuul-gateway-server")
 public interface CurrencyExchangeServiceProxy {
 
-    @GetMapping("/currency-exchange/{from}/to/{to}")
+    //@GetMapping("/currency-exchange/{from}/to/{to}")
+    @GetMapping("/currency-exchange-service/currency-exchange/{from}/to/{to}") // through the Zuul proxy
     CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 
 }
