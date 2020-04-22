3dc513effcec27f1981b5e7a235d927c8a4814ba
serg <serj.teplov@gmail.com>
2020-04-22 07:50:02 +0300

    aws deployment caveats

--- src/main/java/ru/serj/currencyconversionservice/gateway/ExchangeProxyUri.java
+++ src/main/java/ru/serj/currencyconversionservice/gateway/ExchangeProxyUri.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import ru.serj.currencyconversionservice.model.CurrencyConversionBean;
 
+// при исользовании имени random gang, вызов метода не будет трассироваться и логироваться sleuth
 @FeignClient(name = "random-gang", url = "${currency.exchange.service.service.host}:8000") // from kubernetes engine (automatically injects CURRENCY_EXCHANGE_SERVICE_SERVICE_HOST into container)
 //@FeignClient(name = "random-gang", url = "${currency.exchange.service.uri}:8000") // from application.properties or deployment.yml
 public interface ExchangeProxyUri {
